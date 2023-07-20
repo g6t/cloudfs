625588d8e28118a42379a1e86f48dabd4ec8c3e5
idmn <ya.domin@gmail.com>
2023-07-20 12:17:17 +0000

    update s3_setup and s3_show to use `root` instead of `project`

--- R/s3_show.R
+++ R/s3_show.R
@@ -1,20 +1,45 @@
-#' @title Open a path specified by a prefix in bucket-name bucket
+#' @description Given a path like "//my_bucket/project///data"
+#' 1. Replace all repeating all slashes with single slashes.
+#' 2. Remove heading slashes.
+#' 3. Extract `bucket` as the first token of the path
+#' 4. Extract `prefix` as the remaining part.
+#' 5. Return `bucket` and `prefix` in a named list
+#' 
+#' @noRd
+s3_path_to_bucket_prefix <- function(path) {
+  path <- gsub("/+", "/", path)
+  path <- gsub("^/", "", path)
+  bucket <- gsub("\\/.*", "", path)
+  prefix <- substring(path, nchar(bucket) + 2)
+  list(bucket = bucket, prefix = prefix, path = path)
+}
+
+#' @title Browse a path on S3
 #' @description For internal use
 #' @examples 
 #' \dontrun{
-#' # opens bucket-name root
-#' cloud_s3_browse_prefix()
-#' # opens the newsleter folder
-#' cloud_s3_browse_prefix("newsletter/")
+#' # opens list of buckets
+#' cloud_s3_browse_path()
+#' 
+#' # opens bucket called "projects"
+#' cloud_s3_browse_path("projects")
+#' 
+#' # opens folder "alpha" in bucket "projects"
+#' cloud_s3_browse_path("projects/alpha")
 #' }
 #' 
 #' @noRd
-cloud_s3_browse_prefix <- function(prefix = "") {
-  # fix slashes at the beginning and duplicated slashes
-  prefix <- gsub("/+", "/", prefix)
-  url_head <- "https://s3.console.aws.amazon.com/s3/buckets/bucket-name?region=us-east-1&prefix="
-  url_tail <- "&showversions=false"
-  url <- paste0(url_head, prefix, url_tail)
+cloud_s3_browse_path <- function(path = "") {
+  check_string(path)
+  bucket_prefix <- s3_path_to_bucket_prefix(path)
+  
+  url <- "https://s3.console.aws.amazon.com/s3/buckets/"
+  
+  if (bucket_prefix$bucket == "") utils::browseURL(url)
+  
+  url <- paste0(url_head, bucket_prefix$bucket)
+  if (length(prefix) != 0) 
+    url <- paste0(url, "?prefix=", bucket_prefix$prefix, "/")
   utils::browseURL(url)
 }
 
@@ -34,10 +59,11 @@ cloud_s3_browse_prefix <- function(prefix = "") {
 #' 
 #' @export
 cloud_s3_browse <- function(path = "", root = NULL) {
-  stopifnot(is.character(path) & length(path) == 1)
-  s3_folder <- cloud_s3_get_location(project = project)
-  prefix <- file.path(s3_folder, path, "/")
-  cloud_s3_browse_prefix(prefix)
+  check_string(path)
+  check_string(root, alt_null = TRUE)
+  if (is.null(root)) root <- cloud_s3_get_root()
+  full_path <- file.path(root, path, "/")
+  cloud_s3_browse_path(full_path)
 }
 
 #' @title List Contents of Project's S3 Folder
@@ -65,18 +91,16 @@ cloud_s3_browse <- function(path = "", root = NULL) {
 #' 
 #' @export
 cloud_s3_ls <- function(path = "", recursive = FALSE, full_names = FALSE,
-                      project = getwd()) {
-  stopifnot(is.character(path) & length(path) == 1)
-  stopifnot(isTRUE(recursive) | isFALSE(recursive))
-  stopifnot(isTRUE(full_names) | isFALSE(full_names))
+                      root = NULL) {
+  check_string(path)
+  check_bool(recursive)
+  check_bool(full_names)
   
-  s3_location <- cloud_s3_get_location(project = project)
-  s3_path <- clean_file_path(s3_location, path, "/")
-  cli::cli_text("{.field S3 path}: {.path {s3_path}}")
+  if (is.null(root)) root <- cloud_s3_get_root()
+  full_path <- file.path(root, path, "/")
+  bucket_prefix <- s3_path_to_bucket_prefix(full_path)
   
-  s3_path_split <- strsplit(s3_path, "/")[[1]]
-  bucket <- s3_path_split[[1]]
-  prefix <- paste0(paste(s3_path_split[-1], collapse = "/"), "/")
+  cli::cli_text("{.field S3 path}: {.path {bucket_prefix$path}}")
 
   # NOTE: this lists all contents recursively regardless of `recursive`
   # parameter because this way it is easier to parse the response. Shouldn't
@@ -84,16 +108,16 @@ cloud_s3_ls <- function(path = "", recursive = FALSE, full_names = FALSE,
   # with thousands of files (highly unlikely).
   resp_df <- 
     aws.s3::get_bucket_df(
-      bucket = bucket,
+      bucket = bucket_prefix$bucket,
       delimiter = "",
-      prefix = prefix,
+      prefix = bucket_prefix$prefix,
       max = Inf
     )
   
   if (nrow(resp_df) == 0) cli::cli_abort("Path does not exist.")
   
   cont_df <- tibble(.rows = nrow(resp_df))
-  cont_df$short_name <- gsub(paste0("^", prefix), "", resp_df$Key)
+  cont_df$short_name <- gsub(paste0("^", bucket_prefix$prefix), "", resp_df$Key)
   cont_df$last_modified <- 
     as.POSIXct(resp_df$LastModified, format = "%Y-%m-%dT%H:%M:%OS", tz = "EST")
   cont_df$size_b <- as.integer(resp_df$Size)
