331944c5ae30761598da61c32acdb0af81e5a9d9
idmn <ya.domin@gmail.com>
2023-08-26 07:54:47 +0000

    import dplyr::across

--- R/package.R
+++ R/package.R
@@ -1,5 +1,5 @@
 #' @importFrom dplyr `%>%` mutate select group_by summarise left_join case_when
-#'   tibble if_else relocate filter rename
+#'   tibble if_else relocate filter rename across
 #' 
 #' @keywords internal
 "_PACKAGE"
